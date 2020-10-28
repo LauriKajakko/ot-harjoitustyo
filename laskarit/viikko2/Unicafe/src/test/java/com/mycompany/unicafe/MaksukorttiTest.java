98dc7d1a3f829be269772bf57d5421b98e266e82
LauriKajakko <kajakko.lauri@gmail.com>
2020-10-28 10:17:32 +0200

    viikko2 t2

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,49 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void saldoAlussaOikein() {
+        assertEquals(kortti.saldo(), 10);
+    }
+    
+    @Test
+    public void saldonKasvattaminenToimii() {
+        kortti.lataaRahaa(100);
+        assertEquals(kortti.saldo(), 110);
+    }
+    
+    @Test
+    public void negatiivisenLisaaminenEiOnnistu() {
+        kortti.lataaRahaa(-1);
+        assertEquals(kortti.saldo(), 10);
+    }
+    
+    @Test
+    public void saldoVaheneeOtettaessa() {
+        kortti.otaRahaa(5);
+        assertEquals(kortti.saldo(), 5);
+    }
+    
+    @Test
+    public void saldoEiVaheneOtettaessaLiikaa() {
+        kortti.otaRahaa(100500);
+        assertEquals(kortti.saldo(), 10);
+    }
+    
+    @Test
+    public void saldoEiKasvaOtettaessaNeg() {
+        kortti.otaRahaa(-100500);
+        assertEquals(kortti.saldo(), 10);
+    }
+    
+    @Test
+    public void metodiPalauttaaTrue() {
+        assertEquals(kortti.otaRahaa(1), true);
+    }
+    
+    @Test
+    public void metodiPalauttaaFalse() {
+        assertEquals(kortti.otaRahaa(100500), false);
+    }
 }
